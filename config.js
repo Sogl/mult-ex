842a29f546eab656b6fc42c8938428d8055d84f5
davemackintosh <me@davemackintosh.co.uk>
2016-01-27 15:38:18 +0000

    Made config file less scary
    Signed-off-by: davemackintosh <me@davemackintosh.co.uk>

--- config.js
+++ config.js
@@ -33,11 +33,17 @@ module.exports = {
   },
 
   // Configure the Hapi server.
+  // These objects are passed directly to Hapi
+  // and are not abstracted at all, all config
+  // available to Hapi are available to Multicolour.
   api_connections: {
     port: 1811,
     routes: {
       cors: {
-        origin: [ "http://localhost:1811" ]
+        // You should update this to reflect only
+        // the domains you wish to allow access to
+        // the API being generated.
+        origin: [ "*" ]
       }
     },
     router: { stripTrailingSlash: true }
@@ -52,6 +58,12 @@ module.exports = {
     debug: { request: ["error"] }
   },
 
+  // Configure your connections here,
+  // if you do not specify a connection
+  // in your blueprint, Multicolour will
+  // connect to whatever the value of
+  // process.env.NODE_ENV.toLowerCase() is
+  // which in most cases is "development"
   db: {
     adapters: {
       development: require("sails-mongo")
